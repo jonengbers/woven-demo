eea2e04fbf0378f6dd0204257604821c93ffb9c3
jonengbers <jon.engbers@gmail.com>
2026-04-11 21:07:40 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,2 @@
+const SUPABASE_URL  = 'https://weptedvdrzfaulbnimmf.supabase.co';
+const SUPABASE_KEY  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndlcHRlZHZkcnpmYXVsYm5pbW1mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5MzAyNTIsImV4cCI6MjA5MTUwNjI1Mn0.j64AZCtUBKjT2ySMlHZqbaal1rWwL7jCSX_BNqNNphI';
